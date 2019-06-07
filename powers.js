ea4c0a462b6433f1ee938754e898cf41e1dee5c7
joekennerly <joseph.kennerly@gmail.com>
2019-06-07 15:41:57 -0500

    activate!

---
+++ powers.js
@@ -0,0 +1,55 @@
+let flight = document.querySelector('#flight')
+let xray = document.querySelector('#xray')
+let mindreading = document.querySelector('#mindreading')
+
+// console.log(xray.classList.contains())
+
+document.querySelector("#activate-flight").addEventListener("click", () => {
+  if(flight.classList.contains('disabled')){
+    flight.classList.toggle("enabled")
+  }
+})
+
+document.querySelector("#activate-mindreading").addEventListener("click", () => {
+  if(mindreading.classList.contains('disabled')){
+    mindreading.classList.toggle("enabled")
+  }
+})
+
+document.querySelector("#activate-xray").addEventListener("click", () => {
+  if(xray.classList.contains('disabled')){
+    xray.classList.toggle("enabled")
+  }
+})
+
+document.querySelector('#activate-all').addEventListener("click", () => {
+
+  if(xray.classList.contains('disabled')){
+    xray.classList.remove("disabled")
+    xray.classList.add("enabled")
+  }
+  if(mindreading.classList.contains('disabled')){
+    mindreading.classList.remove("disabled")
+    mindreading.classList.add("enabled")
+  }
+  if(flight.classList.contains('disabled')){
+    flight.classList.remove("disabled")
+    flight.classList.add("enabled")
+  }
+})
+
+document.querySelector('#deactivate-all').addEventListener("click", () => {
+  console.log(`deactivate all powers`);
+  if(xray.classList.contains('enabled')){
+    xray.classList.remove("enabled")
+    xray.classList.add("disabled")
+  }
+  if(mindreading.classList.contains('enabled')){
+    mindreading.classList.remove("enabled")
+    mindreading.classList.add("disabled")
+  }
+  if(flight.classList.contains('enabled')){
+    flight.classList.remove("enabled")
+    flight.classList.add("disabled")
+  }
+})
